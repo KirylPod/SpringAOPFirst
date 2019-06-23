3ca8c5be68fad337a7cd85f0406c8b3eb48182cf
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-06-23 18:16:07 +0300

    AOPFirst

--- src/main/java/by/java/training/spring/main/Start.java
+++ src/main/java/by/java/training/spring/main/Start.java
@@ -1,3 +1,5 @@
+package by.java.training.spring.main;
+
 import by.java.training.spring.objects.SomeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -5,9 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Start {
 
     public static void main(String[] args){
+
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         SomeService service = (SomeService) context.getBean("someService");
         double val = service.getDoubleValue();
+        System.out.println(val);
     }
 
 }
